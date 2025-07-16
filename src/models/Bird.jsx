da638094c3d15b291d2fa309b2a6f3eec9000457
Eric Li <li.eric.w@gmail.com>
2025-07-16 11:06:01 -0500

    models/contact

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -2,6 +2,7 @@ import { useRef, useEffect } from 'react'
 
 import birdScene from '../assets/3d/bird.glb';
 import { useGLTF, useAnimations } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 
 const Bird = () => {
   const { scene, animations} = useGLTF(birdScene)
@@ -11,6 +12,25 @@ const Bird = () => {
   useEffect(() => {
     actions['Take 001'].play();
   }, []);
+
+  useFrame(({ clock, camera }) => {
+    // Update the Y position simulate the flight moving in a sin wave
+    birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
+
+    if (birdRef.current.position.x > camera.position.x + 10) {
+      birdRef.current.rotation.y = Math.PI;
+    } else if (birdRef.current.position.x < camera.position.x - 10) {
+      birdRef.current.rotation.y = 0;
+    }
+
+    if (birdRef.current.rotation.y === 0) {
+      birdRef.current.position.x += 0.01;
+      birdRef.current.position.z -= 0.01;
+    } else {
+      birdRef.current.position.x -= 0.01;
+      birdRef.current.position.z += 0.01;
+    }
+  });
 
   return (
     <mesh 
